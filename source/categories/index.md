fe2c274c299b29eace3958340648d87e4d7793ac
allen <eepson123@gmail.com>
2023-04-10 22:08:19 +0800

    feat:add init

--- source/categories/index.md
+++
@@ -1,4 +0,0 @@
----
-title: categories
-date: 2022-10-07 22:54:26
----
--- themes/keep/.stylelintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    'stylelint-config-rational-order',
-    'stylelint-stylus/standard'
-  ],
-  rules: {
-    'stylus/pythonic': 'never'
-  }
-}
