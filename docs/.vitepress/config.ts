8ccfab6224df9d58d242f948fbf3ad66c9cff838
allen <eepson123@gmail.com>
2023-04-10 23:40:40 +0800

    feat:Add the google meta

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,15 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Allen's blog",
   description: 'A VitePress Site',
+  head: [
+    [
+      'meta',
+      {
+        name: 'google-site-verification',
+        content: 'SH2b8gzgTJT_CWEMppSWf_YLQ130Q9PDunRkjaf6EM'
+      }
+    ]
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
