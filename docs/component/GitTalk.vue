c5c003b7cdf709c43e37476f16cbdbbe22d413cc
allen <eepson123@gmail.com>
2023-04-17 22:31:08 +0800

    fix:fix the GitTalk repo Name

--- docs/component/GitTalk.vue
+++ docs/component/GitTalk.vue
@@ -10,7 +10,7 @@ onMounted(() => {
     enable: true,
     clientID: 'deceb2d7071942b4ff48',
     clientSecret: 'b928a71e4e41bc0991623142505926522a68d8d8',
-    repo: 'https://github.com/eepson123tw/blog',
+    repo: 'blog',
     owner: 'eepson123tw',
     admin: ['eepson123tw'],
     githubID: 'eepson123tw',
