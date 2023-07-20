9e0dad4b9e38f9b6b82879585a51072823094366
allen <eepson123@gmail.com>
2023-07-21 02:57:31 +0800

    config:change about me url

--- docs/router/index.ts
+++ docs/router/index.ts
@@ -4,7 +4,7 @@ import smallTalkRouter from './smallTalk'
 
 export const nav = [
   { text: 'Home', link: '/' },
-  { text: 'AboutMe', link: 'https://r3f.zeabur.app/portfolio' },
+  { text: 'AboutMe', link: 'https://ph-portfolio.zeabur.app/' },
   { text: 'Categories', link: '/view/categories' },
   {
     text: 'FrontEnd',
