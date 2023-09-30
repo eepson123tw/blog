066840380c6499734812f0b909d23b864c703c1d
allen <eepson123@gmail.com>
2023-09-30 11:28:32 +0800

    feat(change-file-name): remove unknown file name and change to the meaning name.

--- docs/router/smallTalk.ts
+++ docs/router/smallTalk.ts
@@ -1,15 +1,18 @@
 const smallTalkRouter = {
-  text: 'SmallTalk',
+  text: "SmallTalk",
   items: [
-    { text: '前端轉職很簡單?', link: '/view/posts/frontend-001' },
-    { text: '轉職前端前，你可能會想知道?', link: '/view/posts/frontend-002' },
-    { text: '面對複雜的需求時，應該...?', link: '/view/posts/frontend-007' },
+    { text: "前端轉職很簡單?", link: "/view/posts/change-job-smalltalk" },
+    { text: "轉職前端前，你可能會想知道?", link: "/view/posts/frontend-job-smalltalk" },
     {
-      text: 'ChatGPT 橫空出世，工程師該如何看待這現象?',
-      link: '/view/posts/frontend-008'
-    }
+      text: "面對複雜的需求時，應該...?",
+      link: "/view/posts/complexible-requirement-smalltalk",
+    },
+    {
+      text: "ChatGPT 橫空出世，工程師該如何看待這現象?",
+      link: "/view/posts/chatgpt-smalltalk",
+    },
   ],
-  collapsed: true
-}
+  collapsed: true,
+};
 
-export default smallTalkRouter
+export default smallTalkRouter;
