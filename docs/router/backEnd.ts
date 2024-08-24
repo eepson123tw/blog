fe2e9dc7d60a8e24ade722c76d0a2b37e6ee965a
allen <eepson123@gmail.com>
2024-08-24 22:39:02 +0800

    feat(docker): add the new post about docker

--- docs/router/backEnd.ts
+++ docs/router/backEnd.ts
@@ -4,6 +4,7 @@ const backendRouter = {
     { text: "JWT機制(JSON Web Tokens)", link: "/view/backend/jwt" },
     { text: "Nginx是甚麼?", link: "/view/backend/nginx" },
     { text: "Encode、Decode、Hash 是幹甚麼東西?", link: "/view/backend/encode" },
+    { text: "利用AI學習Docker並應證", link: "/view/backend/docker" },
   ],
   collapsed: true,
 };
